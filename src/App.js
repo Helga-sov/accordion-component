d67f1af4780c632a4194af8f3bf49b26ebb93078
Olga <sandu.helga@gmail.com>
2023-07-04 14:36:24 +0200

    implemented children prop inside AccordionItem component

--- src/App.js
+++ src/App.js
@@ -28,16 +28,28 @@ export default function App() {
 }
 
 function Accordion({data}) {
-  return <div className="accordion">
-  {data.map((el, i) => <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />)}
-  </div>;
+  const [curOpen, setCurOpen] = useState(null);
+
+  return (
+  <div className="accordion">
+  {data.map((el, i) => (
+      <AccordionItem 
+        curOpen={curOpen} 
+        onOpen={setCurOpen} 
+        title={el.title} 
+        num={i} 
+        key={el.title} 
+      >{el.text}</AccordionItem>
+      ))}
+  </div>
+  );
 }
 
-function AccordionItem({num, title, text}) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({num, title, curOpen, onOpen, children}) {
+  const isOpen = num === curOpen;
 
   function handleToggle() {
-    return setIsOpen(!isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return <div className={`item ${isOpen ? 'open' : ''}`} onClick={handleToggle}>
@@ -45,6 +57,6 @@ function AccordionItem({num, title, text}) {
     <p className="title">{title}</p>
     <p className="icon">{isOpen ? `-` : `+`}</p>
 
-    {isOpen && <div className="content-box">{text}</div>}
+    {isOpen && <div className="content-box">{children}</div>}
   </div>
 }
